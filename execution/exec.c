71c138712f398fe6f8977ace8b9fb9ad68bf4862
zizo <elgrandesimo53@gmail.com>
2023-03-09 23:25:35 +0100

    need to fixe something bach yb9a yquite

--- execution/exec.c
+++ execution/exec.c
@@ -6,7 +6,7 @@
 /*   By: moazzedd <moazzedd@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 21:41:38 by moazzedd          #+#    #+#             */
-/*   Updated: 2023/03/09 16:20:27 by moazzedd         ###   ########.fr       */
+/*   Updated: 2023/03/09 18:42:29 by moazzedd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,50 @@
 
 void	one_exec(t_all *lexer)
 {
-    // pid_t pid;
     int i;
 	
 	i = 0;
-    	// pid = fork();
-		while (lexer)
+	while (lexer)
+	{
+		while (lexer->cmd[i])
 		{
-			while (lexer->cmd[i])
-			{
-				execvp(lexer->cmd[i], lexer->cmd);
-				i++;
-			}
-			i = 0;
-			lexer = lexer->next;
+			execvp(lexer->cmd[i], lexer->cmd);
+			i++;
 		}
-		// }
-		// else
-		// 	waitpid(pid, &i, i);
-		// i = 0;
+		i = 0;
+		lexer = lexer->next;
+	}
 }
 
+void	two_exec(t_all *lexer)
+{
+	int pipefd[2];
+	pid_t pid = fork();
+	if (pid == 0)
+	{
+    	close(pipefd[0]);
+    	dup2(pipefd[1], STDOUT_FILENO);
+		close(pipefd[1]);
+        execvp(lexer->cmd[0], lexer->cmd);
+    }
+	else
+	{
+        close(pipefd[1]);
+		dup2(pipefd[0], STDIN_FILENO);
+		close(pipefd[0]);
+        waitpid(pid, NULL, 0);
+        execvp(lexer->next->cmd[0], 	lexer->next->cmd);
+    }
+}
 void    ft_exec(t_all *lexer)
 {
 	int pipefd[2];
 	pipe(pipefd);
 
+    pid_t pid = fork();
 	if (lexer->next == NULL)
 		one_exec(lexer);
-    pid_t pid = fork();
+	printf("------------> %d\n",pid);
     if (pid == -1)
         perror("fork");
 	else
@@ -62,11 +77,10 @@ void    ft_exec(t_all *lexer)
 				dup2(pipefd[0], STDIN_FILENO);
 				close(pipefd[0]);
     		    waitpid(pid, NULL, 0);
-    		    execvp(lexer->next->cmd[0], 	lexer->next->cmd);
+    		    execvp(lexer->next->cmd[0], lexer->next->cmd);
     		}
 			lexer = lexer->next;
 		}
-		// exit(0);
 	}
 
 
